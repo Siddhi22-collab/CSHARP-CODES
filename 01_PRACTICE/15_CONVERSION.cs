4821ae707a690a69131372520a0f8e70d5d196dc
Siddhi Lokhande <siddhilokhande2202@gmail.com>
2025-02-17 22:20:44 +0530

    17 Feb 2025

---
+++ 01_PRACTICE/15_CONVERSION.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Geeks
+{
+    static public void Main () {
+      
+      String Strnum = "123";
+      
+      // Converts string to integer
+      int num = Convert.ToInt32(Strnum); 
+      
+      Console.WriteLine(Strnum+ " Coverted to Integer: "+ 
+      num );
+    }
+}
