613d015834136cdf2f851536cb7952bb347b4572
Siddhi Lokhande <siddhilokhande2202@gmail.com>
2025-02-27 22:28:52 +0530

    GET CURRENT DATE

---
+++ 01_PRACTICE/25_GETCURRENTDATE.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        DateTime currentDate = DateTime.Today;
+
+        Console.WriteLine("Current Date: " + currentDate.ToString("d")); // Prints the date in short format
+
+    }
