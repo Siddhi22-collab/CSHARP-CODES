8a143cef227b8aae5b3e9938265cda6732cd5f4d
Siddhi Lokhande <siddhilokhande2202@gmail.com>
2025-03-10 19:35:44 +0530

    SWITCH STATEMENT

---
+++ 01_PRACTICE/33_SWITCHSTATEMENT.cs
@@ -0,0 +1,28 @@
+using System; 
+
+public class Geeks 
+{ 
+    // Main Method 
+    public static void Main(String[] args) 
+    { 
+        int nitem = 5; 
+        switch (nitem) { 
+
+        case 1: 
+            Console.WriteLine("Case 1"); 
+            break; 
+
+        case 5: 
+            Console.WriteLine("Case 5"); 
+            break; 
+
+        case 9: 
+            Console.WriteLine("Case 9"); 
+            break; 
+
+        default: 
+            Console.WriteLine("No match found"); 
+            break; 
+        } 
+    } 
+} 
